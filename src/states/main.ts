6799b16c1c279a5d6b81d90bd1a9cc6cf286ba7c
Denis Moiseev <isdenmois@gmail.com>
2018-11-16 14:19:28 +0300

    Modal stack

--- src/states/main.ts
+++ src/states/main.ts
@@ -6,15 +6,24 @@ import { LoginScreen } from 'views/login/LoginScreen'
 import { ProfileScreen } from 'views/profile/ProfileScreen'
 import { BooksSearchScreen } from 'views/books-search/BooksSearchScreen'
 
+export const ModalStack = {
+}
+
 export const AppStack = createStackNavigator(
   {
     Home: HomeScreen,
     Book: BookScreen,
     Profile: ProfileScreen,
     BooksSearch: BooksSearchScreen,
+    ...ModalStack,
   },
   {
     initialRouteName: 'Home',
+    mode: 'card',
+    cardStyle: {
+      backgroundColor: 'transparent',
+      opacity: 1,
+    },
   },
 )
 
