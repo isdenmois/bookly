c89750900de231b2514a2c3dfeb9a185cfafa347
Denis Moiseev <isdenmois@gmail.com>
2018-11-19 21:50:05 +0300

    Query list spinner

--- server/response.js
+++ server/response.js
@@ -1,7 +1,11 @@
 exports.ok = (res, data, count) => {
-  res.send({
+  setTimeout(() => res.send({
     status: {code: 200, message: 'OK'},
     data,
     count
-  });
+  }), delay());
 };
+
+function delay() {
+  return Math.round(3000 * Math.random())
+}
--- src/components/query-list.tsx
+++ src/components/query-list.tsx
@@ -1,69 +1,81 @@
 import * as _ from 'lodash'
 import * as React from 'react'
-import { ActivityIndicator, Text } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, TextStyle, ViewStyle } from 'react-native'
 import { DocumentNode } from 'graphql'
 import { Query } from 'react-apollo'
 import { Button, List, ListItem } from 'native-base'
+import { color } from 'constants/colors'
 
 interface Props {
-    query: DocumentNode
-    variables?: any
-    emptyComponent: any
-    itemComponent: any
-    resultCountComponent?: any
-    request: string
-    field: string
+  query: DocumentNode
+  variables?: any
+  emptyComponent: any
+  itemComponent: any
+  resultCountComponent?: any
+  request: string
+  field: string
 }
 
-export class QueryList extends React.Component<Props> {
-    render() {
-        return (
-            <Query query={this.props.query} variables={this.props.variables}>
-              {this.renderResult}
-            </Query>
-        )
-    }
+type State = {
+  refresh: boolean
+}
 
-    renderResult = ({ loading, data, fetchMore }) => {
-      const { emptyComponent: Empty, itemComponent: Item, resultCountComponent: ResultCount, request, field } = this.props,
-            items = _.get(data, [request, field])
+export class QueryList extends React.Component<Props, State> {
+  state: State = {refresh: false}
 
-      if (loading && !items) return <ActivityIndicator size='large'/>
-      if (!loading && _.isEmpty(items)) return <Empty/>
+  render() {
+    return (
+        <Query query={this.props.query} variables={this.props.variables}>
+          {this.renderResult}
+        </Query>
+    )
+  }
 
-      return (
-        <List>
-          {ResultCount &&
-            <ListItem first>
-              <ResultCount count={_.get(data, [request, 'count'])}/>
-            </ListItem>
-          }
-          {_.map(items, item =>
-            <ListItem key={item.id}>
-              <Item item={item}/>
-            </ListItem>
-          )}
+  renderResult = ({ loading, data, fetchMore }) => {
+    const { emptyComponent: Empty, itemComponent: Item, resultCountComponent: ResultCount, request, field } = this.props,
+          items = _.get(data, [request, field])
 
-          {loading && <ActivityIndicator size='large'/>}
-          {!loading && this.renderFetchMore(fetchMore, data[request], items)}
-        </List>
-      )
-    }
+    if (loading) return <ActivityIndicator size='large'/>
+    if (!loading && _.isEmpty(items)) return <Empty/>
 
-    renderFetchMore(fetchMore, data, items) {
-      if (_.size(items) >= data.count) return null
+    return (
+      <List>
+        {ResultCount &&
+          <ListItem first>
+            <ResultCount count={_.get(data, [request, 'count'])}/>
+          </ListItem>
+        }
+        {_.map(items, item =>
+          <ListItem key={item.id}>
+            <Item item={item}/>
+          </ListItem>
+        )}
 
-      return (
-        <Button full onPress={() => this.fetchMoreParams(fetchMore, _.size(items) + 1)}>
-          <Text style={{color: 'white'}}>Загрузить еще</Text>
-        </Button>
-      )
-    }
+        {this.state.refresh && <ActivityIndicator size='large'/>}
+        {!this.state.refresh && this.renderFetchMore(fetchMore, data[request], items)}
+      </List>
+    )
+  }
 
-    fetchMoreParams = (fetchMore, start) => fetchMore({
+  renderFetchMore(fetchMore, data, items) {
+    if (_.size(items) >= data.count) return null
+
+    return (
+      <Button style={s.button} full onPress={() => this.fetchMoreParams(fetchMore, _.size(items) + 1)}>
+        <Text style={s.buttonText}>Загрузить еще</Text>
+      </Button>
+    )
+  }
+
+  fetchMoreParams = (fetchMore, start) => {
+    this.setState({refresh: true})
+
+    fetchMore({
       variables: {start},
       updateQuery: updateQueryList(this.props.request, this.props.field),
     })
+      .finally(() => this.setState({refresh: false}))
+  }
 }
 
 function updateQueryList(request, field) {
@@ -77,3 +89,13 @@ function updateQueryList(request, field) {
     })
   }
 }
+
+const s = StyleSheet.create({
+  button: {
+    backgroundColor: color.Green,
+  } as ViewStyle,
+
+  buttonText: {
+    color: 'white',
+  } as TextStyle,
+})
