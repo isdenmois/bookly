68cc0e1111d1d612ec5413b074acb1205e7c0927
Denis Moiseev <isdenmois@gmail.com>
2019-01-13 20:07:09 +0300

    Date of read

--- src/views/book-list/components/have-read-item.tsx
+++ src/views/book-list/components/have-read-item.tsx
@@ -7,6 +7,7 @@ import { BookS } from 'models'
 
 import { Rating } from 'components/rating'
 import { TextM, TextS } from 'components/text'
+import { DatePicker } from 'native-base';
 
 interface Props {
   book: BookS
@@ -28,7 +29,10 @@ export class HaveReadItem extends React.PureComponent<Props> {
           <TextM>{book.title}</TextM>
           <TextS style={s.author}>{book.authorsName}</TextS>
 
-          <Rating scale={5} value={book.rating}/>
+          <View style={s.readInfo}>
+            <TextM style={s.date}>{book.date.toLocaleDateString()}</TextM>
+            <Rating scale={5} value={book.rating}/>
+          </View>
         </View>
       </View>
     )
@@ -44,6 +48,11 @@ const s = StyleSheet.create({
     marginRight: 10,
   } as ViewStyle,
 
+  readInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  } as ViewStyle,
+
   infoContainer: {
     flexDirection: 'column',
     flex: 1,
@@ -51,5 +60,10 @@ const s = StyleSheet.create({
 
   author: {
     color: '#424242',
+  } as TextStyle,
+
+  date: {
+    color: '#424242',
+    marginRight: 10,
   } as TextStyle,
 })
