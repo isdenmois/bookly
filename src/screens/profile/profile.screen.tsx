cb966fbb5d2c420403edd716ba9b74b05a15f29d
Denis Moiseev <isdenmois@gmail.com>
2019-06-20 16:38:52 +0300

    Profile remarkup

--- src/screens/profile/profile.screen.tsx
+++ src/screens/profile/profile.screen.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { NavigationScreenProps } from 'react-navigation';
 import { StyleSheet, Text, View, ViewStyle, TextStyle } from 'react-native';
 import { inject, InjectorContext } from 'react-ioc';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { Database } from '@nozbe/watermelondb';
 import { Session } from 'services';
-import { ListItem } from 'components/list-item';
+import { TouchIcon } from 'components/touch-icon';
+import { Button } from 'components/button';
 
 export class ProfileScreen extends React.Component<NavigationScreenProps> {
   static contextType = InjectorContext;
@@ -15,11 +17,31 @@ export class ProfileScreen extends React.Component<NavigationScreenProps> {
   render() {
     return (
       <View style={s.container}>
-        <Text style={s.profile}>{this.session.userId}</Text>
-        <ListItem last label='Выйти' onPress={this.logout} />
+        <View style={s.header}>
+          <TouchIcon
+            paddingHorizontal={10}
+            paddingVertical={10}
+            name='arrow-left'
+            size={24}
+            color='#000'
+            onPress={this.goBack}
+          />
+          <Text style={s.profile}>{this.session.userId}</Text>
+        </View>
+        <View style={s.buttonContainer}>
+          <Button
+            label='Выйти'
+            onPress={this.logout}
+            icon={<Icon name='sign-out-alt' size={18} color='black' />}
+            style={s.button}
+            textStyle={s.buttonText}
+          />
+        </View>
       </View>
     );
   }
+
+  goBack = () => this.props.navigation.pop();
 
   logout = () => {
     this.session.stopSession();
@@ -33,9 +55,29 @@ const s = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   } as ViewStyle,
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  } as ViewStyle,
   profile: {
+    flex: 1,
+    marginRight: 44,
     fontSize: 24,
     textAlign: 'center',
+    color: 'black',
+  } as TextStyle,
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 24,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+  } as ViewStyle,
+  button: {
+    backgroundColor: 'white',
+    elevation: 3,
+  } as ViewStyle,
+  buttonText: {
     color: 'black',
   } as TextStyle,
 });
