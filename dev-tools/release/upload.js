032a49b70b0b22407463861630fd5f4fb57a161a
Denis Moiseev <isdenmois@gmail.com>
2019-08-02 14:34:00 +0300

    Replace fastlane with custom build script

---
+++ dev-tools/release/upload.js
@@ -0,0 +1,12 @@
+const Diawi = require('diawi');
+
+exports.upload = function upload(path) {
+  return new Promise((resolve, reject) => {
+    console.log('Start upload apk');
+
+    new Diawi({ path, token: process.env.DIAWI_TOKEN, callback_emails: process.env.DIAWI_EMAILS })
+      .on('complete', resolve)
+      .on('error', reject)
+      .execute();
+  });
+};
