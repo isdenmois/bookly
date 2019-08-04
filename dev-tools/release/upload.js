c84d341785a4cbc1effe95b955bec3c527a8a9c7
Denis Moiseev <isdenmois@gmail.com>
2019-08-04 09:32:39 +0300

    Rename apk before upload

--- dev-tools/release/upload.js
+++ dev-tools/release/upload.js
@@ -1,8 +1,13 @@
 const Diawi = require('diawi');
+const npath = require('path');
+const fs = require('fs');
+const { name, version } = require('../../package.json');
 
 exports.upload = function upload(path) {
   return new Promise((resolve, reject) => {
     console.log('Start upload apk');
+
+    path = renameApk(path);
 
     new Diawi({ path, token: process.env.DIAWI_TOKEN, callback_emails: process.env.DIAWI_EMAILS })
       .on('complete', resolve)
@@ -10,3 +15,13 @@ exports.upload = function upload(path) {
       .execute();
   }).then(url => console.log('Successfully uploaded to', url));
 };
+
+function renameApk(path) {
+  const newPath = npath.join(npath.dirname(path), `${name}-${version}.apk`);
+
+  if (newPath !== path) {
+    fs.renameSync(path, newPath);
+  }
+
+  return newPath;
+}
