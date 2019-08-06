0c7e63146330a328096675b47faa78885cd17f28
Denis Moiseev <isdenmois@gmail.com>
2019-08-06 12:30:38 +0300

    Added why-did-you-update

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ if (!global.Proxy) {
 
 AppRegistry.registerComponent(appName, () => require('./config/root/root').default);
 
+// if (__DEV__) {
+//   console.disableYellowBox = true;
+//   require('why-did-you-update').whyDidYouUpdate(require('react'), { exclude: /^YellowBox/ });
+// }
+
 // if (__DEV__ && Platform.OS !== 'web') {
 //   require('./dev-tools/fetch');
 //   //  не забудь adb reverse tcp:9090 tcp:9090
