8719dd29032c17478859bfc95b30ecf97169070c
Denis Moiseev <isdenmois@gmail.com>
2019-08-24 17:39:28 +0300

    Join main and details tabs

--- src/screens/details/tabs/details-tab.tsx
+++ src/screens/details/tabs/details-tab.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, ViewStyle, TextStyle } from 'react-native';
+import { Text, View, StyleSheet, ViewStyle, TextStyle, Linking } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 import { color } from 'types/colors';
 import { formatDate } from 'utils/date';
@@ -18,33 +18,41 @@ import { withScroll } from './tab';
 interface Props extends NavigationScreenProps {
   book: Book & BookExtended;
   isExist: boolean;
+  tab: string;
 }
 
 @withScroll
 export class DetailsTab extends React.PureComponent<Props> {
   render() {
     const { book, isExist } = this.props;
+    const all = this.props.tab !== 'main';
+    const withGenre = all || !book.thumbnail;
 
     return (
       <View>
-        <ViewLine first title='Тип' value={BOOK_TYPE_NAMES[book.type]} />
+        {all && <ViewLine title='Тип' value={BOOK_TYPE_NAMES[book.type]} />}
 
-        {!!book.genre && <ViewLine title='Жанр' value={book.genre} />}
-        {!!book.year && <ViewLine title='Год' value={book.year} />}
+        {withGenre && !!book.genre && <ViewLine title='Жанр' value={book.genre} />}
+        {withGenre && !!book.year && <ViewLine title='Год' value={book.year} />}
 
         {book.status === BOOK_STATUSES.READ && <ViewLine title='Дата прочтения' value={formatDate(book.date)} />}
 
-        {!!book.editionCount && <ViewLine title='Изданий' value={book.editionCount} />}
+        {!!book.editionCount && (
+          <ViewLineTouchable title='Изданий' value={book.editionCount} onPress={this.openEditions} />
+        )}
+
         {!!book.language && <ViewLine title='Язык написания' value={book.language} />}
-        {!!book.title && !!book.originalTitle && <ViewLine title='Оригинальное название' value={book.originalTitle} />}
+        {!!book.title && !!book.originalTitle && (
+          <ViewLineTouchable title='Оригинальное название' value={book.originalTitle} onPress={this.openTelegram} />
+        )}
 
         {!!book.otherTitles && <ViewLine title='Другие названия' value={book.otherTitles} />}
 
-        {!!book.description && <BookDescriptionLine description={book.description} />}
+        {all && !!book.description && <BookDescriptionLine description={book.description} />}
 
         {!!book.parent.length && this.renderParentBooks()}
 
-        {isExist && <ViewLineModelRemove model={book} warning='Удалить книгу из коллекции' />}
+        {all && isExist && <ViewLineModelRemove model={book} warning='Удалить книгу из коллекции' />}
       </View>
     );
   }
@@ -64,6 +72,13 @@ export class DetailsTab extends React.PureComponent<Props> {
   openBook(book) {
     this.props.navigation.push('Details', { bookId: book.id });
   }
+
+  openEditions = () => {
+    const { editionIds, translators } = this.props.book;
+    this.props.navigation.push('Editions', { editionIds, translators });
+  };
+
+  openTelegram = () => Linking.openURL(`tg://share?text=${this.props.book.originalTitle}`);
 }
 
 const s = StyleSheet.create({
@@ -72,6 +87,6 @@ const s = StyleSheet.create({
     fontSize: 14,
   } as TextStyle,
   parentBooks: {
-    marginTop: 15,
+    marginTop: 10,
   } as ViewStyle,
 });
