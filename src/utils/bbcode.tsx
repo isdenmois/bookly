a5148004792ba68a395c7eb0340852cc23faa4fe
Denis Moiseev <isdenmois@gmail.com>
2019-08-24 20:54:27 +0300

    FIxed non-loaded reviews

---
+++ src/utils/bbcode.tsx
@@ -0,0 +1,15 @@
+import { Text } from 'react-native';
+import Parser from 'bbcode-to-react/lib/parser';
+import Tag from 'bbcode-to-react/lib/tag';
+import { SpoilerTag } from 'components';
+
+class TextTag extends Tag {
+  toReact() {
+    return <Text>{super.getComponents()} </Text>;
+  }
+}
+
+export const parser = new Parser([]);
+
+parser.registerTag('spoiler', SpoilerTag);
+parser.registerTag('*', TextTag);
