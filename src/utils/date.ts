1641ba03fb1037d32da575b57a7c4c327c83c36d
Denis Moiseev <isdenmois@gmail.com>
2019-09-11 23:37:52 +0300

    Remove date-fns

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,30 @@
-import format from 'date-fns/format';
+// import format from 'date-fns/format';
 
 const DATE_FORMAT = 'DD.MM.YYYY';
 
+const TOKEN = /MM|DD|YYYY/g;
+
+const flags = {
+  YYYY: (d: Date) => d.getFullYear(),
+  YY: (d: Date) => String(d.getFullYear()).slice(2),
+  MM: (d: Date) => p(d.getMonth() + 1),
+  DD: (d: Date) => p(d.getDate()),
+  HH: (d: Date) => p(d.getHours()),
+  mm: (d: Date) => p(d.getMinutes()),
+};
+
+export function format(date: any, mask: string) {
+  if (!(date instanceof Date)) {
+    date = new Date(date);
+  }
+
+  return mask.replace(TOKEN, $0 => flags[$0](date));
+}
+
 export function formatDate(date) {
   return format(date, DATE_FORMAT);
+}
+
+function p(s: number) {
+  return String(s).padStart(2, '0');
 }
