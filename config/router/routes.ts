a0ef1d85a5a0220369cbad35e4b031e1c5a08f67
Denis Moiseev <isdenmois@gmail.com>
2019-09-22 15:15:05 +0300

    feat: Added webview for books upload

--- config/router/routes.ts
+++ config/router/routes.ts
@@ -6,6 +6,7 @@ import { ProfileScreen } from 'screens/profile/profile.screen';
 import { DetailsScreen } from 'screens/details/details.screen';
 import { EditionsListScreen } from 'screens/editions/editions.screen';
 import { BookSelectScreen } from 'screens/book-select/book-select.screen';
+import { ReaderManagerScreen } from 'screens/sync/reader-manager.screen';
 
 import { ChangeStatusModal } from 'modals/change-status/change-status.modal';
 import { BookSelectModal } from 'modals/book-select/book-select.modal';
@@ -14,6 +15,7 @@ import { ThumbnailSelectModal } from 'modals/thumbnail-select/thumbnail-select.m
 import { ReviewWriteModal } from 'modals/review-write/review-write.modal';
 import { FantlabLoginModal } from 'modals/fantlab-login/fantlab-login.modal';
 import { BookTitleEditModal } from 'modals/book-title-edit/book-title-edit.modal';
+import { ScanAddressModal } from 'modals/scan-address/scan-address.modal';
 
 export const MainStack = {
   Home: HomeScreen,
@@ -24,6 +26,7 @@ export const MainStack = {
   Details: DetailsScreen,
   Editions: EditionsListScreen,
   BookSelect: BookSelectScreen,
+  ReaderManager: ReaderManagerScreen,
 };
 
 export const ModalStack = {
@@ -34,4 +37,5 @@ export const ModalStack = {
   ReviewWrite: ReviewWriteModal,
   FantlabLogin: FantlabLoginModal,
   BookTitleEdit: BookTitleEditModal,
+  ScanAddress: ScanAddressModal,
 };
---
+++ src/modals/scan-address/scan-address.modal.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useCallback, useMemo } from 'react';
+import { View, StyleSheet, ViewStyle } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Dialog, ListItem, Button } from 'components';
+import { NavigationStackProp } from 'react-navigation-stack';
+import { color } from 'types/colors';
+import { inject, Session } from 'services';
+
+interface Props {
+  navigation: NavigationStackProp;
+}
+
+export function ScanAddressModal({ navigation }: Props) {
+  const session = useMemo(() => inject(Session), []);
+  const [address, setAddress] = useState(session.lastAddress);
+  const [scan, setScan] = useState(false);
+  const openScan = useCallback(() => setScan(true), []);
+  const openManager = useCallback(() => {
+    if (!address) return;
+
+    session.setLastAddress(address);
+    navigation.goBack();
+    navigation.push('ReaderManager', { address: `http://${address}:8080` });
+  }, [address]); // eslint-disable-line react-hooks/exhaustive-deps
+  const onScan = useCallback(({ data }) => {
+    if (!data) return;
+
+    setScan(false);
+    setAddress(data.replace('http://', '').replace(':8080', ''));
+  }, []);
+
+  return (
+    <Dialog style={s.d} title='Адрес'>
+      {!scan && (
+        <View>
+          <ListItem
+            value={address}
+            onChange={setAddress}
+            onSubmit={openManager}
+            keyboardType='decimal-pad'
+            icon={<Icon name='edit' size={16} color={color.PrimaryText} />}
+          />
+          <ListItem
+            value='Сканировать'
+            icon={<Icon name='qrcode' size={16} color={color.PrimaryText} />}
+            onPress={openScan}
+          />
+        </View>
+      )}
+      {scan && (
+        <View style={s.cameraRow}>
+          <RNCamera
+            style={s.camera}
+            type={RNCamera.Constants.Type.back}
+            captureAudio={false}
+            onBarCodeRead={onScan}
+            onFacesDetected={null}
+            onTextRecognized={null}
+          />
+        </View>
+      )}
+      <Button style={s.button} label='Продолжить' disabled={scan || !address} onPress={openManager} />
+    </Dialog>
+  );
+}
+
+const s = StyleSheet.create({
+  d: {
+    paddingHorizontal: 20,
+  } as ViewStyle,
+  button: {
+    marginTop: 20,
+    marginBottom: 30,
+    alignSelf: 'center',
+  } as ViewStyle,
+  cameraRow: {
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+  } as ViewStyle,
+  camera: {
+    width: 200,
+    height: 200,
+  } as ViewStyle,
+});
