c04f051ff48bb3236e4fecf1a6c608b86ee956d7
Denis Moiseev <isdenmois@gmail.com>
2019-10-25 20:18:02 +0300

    feat(challenges): added plan calculation

--- src/screens/home/home.queries.ts
+++ src/screens/home/home.queries.ts
@@ -1,7 +1,6 @@
 import { Database, Q } from '@nozbe/watermelondb';
 import { BOOK_STATUSES } from 'types/book-statuses.enum';
-
-const ONE_DAY = 1000 * 60 * 60 * 24;
+import { dayOfYear, getStartOfYear } from 'utils/date';
 
 export function booksReadForecast(read: number, total: number): number {
   const yearProgress = dayOfYear() / 365;
@@ -25,16 +24,4 @@ export function readBooksThisYearQuery(database: Database) {
 
 export function readBooksQuery(database: Database) {
   return database.collections.get('books').query(Q.where('status', BOOK_STATUSES.READ));
-}
-
-function getStartOfYear(date = new Date()) {
-  return new Date(date.getFullYear(), 0, 1, 0);
-}
-
-function dayOfYear(): number {
-  const now = new Date();
-  const start = getStartOfYear(now);
-  const diff = now.getTime() - start.getTime() + (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
-
-  return Math.floor(diff / ONE_DAY);
 }
