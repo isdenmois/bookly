f4991213cd60e6cf44999c6e616df86a27589329
Denis Moiseev <isdenmois@gmail.com>
2019-11-11 22:31:16 +0300

    Stat refactor

---
+++ src/screens/stat/tabs/shared.ts
@@ -0,0 +1,60 @@
+import { getCurrentYear } from 'utils/date';
+
+export interface StatBook {
+  year: number;
+  month: number;
+  date: Date;
+  rating: number;
+}
+
+export interface BookItems {
+  items: StatBook[];
+  minYear: number;
+}
+
+export interface IRow {
+  id: number | string;
+  d?: number;
+}
+
+export interface FactoryProps {
+  books: StatBook[];
+  year: number;
+}
+
+export const CURRENT_YEAR = getCurrentYear();
+
+export function byYear(year) {
+  const start = new Date(year, 0, 1, 0, 0, 0);
+  const end = new Date(year, 11, 31, 23, 59, 59);
+
+  return book => book.date >= start && book.date <= end;
+}
+
+export function round(n: number) {
+  return Math.round(n * 10) / 10;
+}
+
+export function dayOfYear() {
+  const start = new Date(CURRENT_YEAR, 0, 0).getTime();
+  const diff = new Date().getTime() - start;
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  return Math.floor(diff / oneDay);
+}
+
+export function mapBooks(items: any[]) {
+  let minYear = CURRENT_YEAR;
+  items = items.map(b => {
+    const year = b.date.getFullYear();
+    const month = b.date.getMonth();
+
+    if (year < minYear) {
+      minYear = year;
+    }
+
+    return { year, month, rating: b.rating, date: b.date };
+  });
+
+  return { items, minYear };
+}
