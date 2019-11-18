b30780310ad9797a3207b54fd9e48de7ac05249f
Denis Moiseev <isdenmois@gmail.com>
2019-11-18 21:41:52 +0300

    Remove e2e from main repo

--- e2e/1-login.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Login page', () => {
-  it('should open home screen after login', async () => {
-    await element(by.id('loginField')).typeText('e2e');
-
-    await element(by.id('submitButton')).tap();
-
-    await expect(element(by.id('homeScreen'))).toBeVisible();
-  });
-});
