aa6e5c07e22a549759babcbb2aeb069a58307623
Denis Moiseev <isdenmois@gmail.com>
2020-01-27 22:05:07 +0300

    rn-v8

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 
-if (!global.Proxy) {
-  require('proxy-polyfill');
-}
-
 AppRegistry.registerComponent(appName, () => require('./config/root/root').default);
 
 // if (__DEV__) {
