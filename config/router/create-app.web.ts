85b661b354fe5affbc99bfd9140eb6143982692a
Denis Moiseev <isdenmois@gmail.com>
2020-02-13 21:01:52 +0300

    Update deps

--- config/router/create-app.web.ts
+++ config/router/create-app.web.ts
@@ -1,3 +1,3 @@
 import { createBrowserApp } from '@react-navigation/web';
 
-export const createApp = createBrowserApp;
+export const createApp = input => createBrowserApp(input, { history: 'hash' });
