2b001bb8e0b1a03e0ecf179170330f103afbe706
Denis Moiseev <isdenmois@gmail.com>
2020-04-18 22:04:21 +0000

    Fixed(web): livelib requests

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,7 @@
 import { FANTLAB_URL as fl, FIREBASE_URL as fb, FANTLAB_ROOT_URL as rfl, LIVELIB_URL as ll } from 'react-native-dotenv';
+import { Platform } from 'react-native';
 
 export const FANTLAB_URL = fl;
 export const FIREBASE_URL = fb;
 export const FANTLAB_ROOT_URL = rfl;
-export const LIVELIB_URL = ll;
+export const LIVELIB_URL = Platform.OS === 'web' ? '/api/livelib' : ll;
