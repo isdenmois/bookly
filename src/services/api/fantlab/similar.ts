d0871986d634283c42c8cf1c67102fe478ba3d27
Denis Moiseev <isdenmois@gmail.com>
2020-04-18 17:40:36 +0000

    Fixed thumbnail id parsing

--- src/services/api/fantlab/similar.ts
+++ src/services/api/fantlab/similar.ts
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { BOOK_TYPES } from 'types/book-types';
 import { api } from '../base/api';
 
-const THUMBNAIL_ID = /(\d+$)/;
+const THUMBNAIL_ID = /(\d+)($|\?)/;
 const types = [BOOK_TYPES.novel, BOOK_TYPES.story, BOOK_TYPES.shortstory];
 
 interface Params {
@@ -12,14 +12,14 @@ interface Params {
 const response = {
   id: 'id',
   title: 'name',
-  author: r => _.map(r.creators?.authors, a => a.name).join(', '),
-  thumbnail: r => r.image?.match(THUMBNAIL_ID)?.['0'] || null,
+  author: (r) => _.map(r.creators?.authors, (a) => a.name).join(', '),
+  thumbnail: (r) => r.image?.match(THUMBNAIL_ID)?.['1'] || null,
   type: 'name_type',
 };
 
 export default api
   .get<Params>('/work/:bookId/similars', true)
-  .filterBefore(row => row?.type === 'work' && types.includes(row.name_type_icon))
+  .filterBefore((row) => row?.type === 'work' && types.includes(row.name_type_icon))
   .response(response);
 
 export interface BookSimilar {
