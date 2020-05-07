f56e2785d7d0253ea8f072dd54ae0628f64e4e9e
Denis Moiseev <isdenmois@gmail.com>
2020-05-07 15:04:00 +0000

    feat(details): Added button "unsee" to book review

--- src/components/expandable-text.tsx
+++ src/components/expandable-text.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
-import { Text, TouchableOpacity, StyleSheet, TextStyle, Insets } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, TextStyle, Insets } from 'react-native';
 import { color } from 'types/colors';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 interface Props {
   numberOfLines?: number;
   children: React.ReactNode;
+  onShrink?: () => void;
 }
 
 const hitSlop: Insets = { top: 20, right: 20, bottom: 20, left: 20 };
 
 export function ExpandableText(props: Props) {
-  const [expanded, setExpanded] = React.useState(false);
-  const toggleExpanded = React.useCallback(() => setExpanded(!expanded), [expanded]);
+  const [expanded, setExpanded] = useState(false);
+  const toggleExpanded = useCallback(() => {
+    if (expanded && props.onShrink) {
+      props.onShrink();
+    }
+    setExpanded(!expanded);
+  }, [expanded]);
   const lines = expanded ? null : props.numberOfLines;
   const onTextPress = expanded ? null : toggleExpanded;
 
   return (
-    <>
-      <Text style={s.body} numberOfLines={lines} onPress={onTextPress}>
-        {props.children}
-      </Text>
+    <View style={s.container}>
+      <View style={{ flex: 1 }}>
+        <Text style={s.body} numberOfLines={lines} onPress={onTextPress}>
+          {props.children}
+        </Text>
 
-      <TouchableOpacity onPress={toggleExpanded} hitSlop={hitSlop}>
-        <Text style={s.toggleText}>{expanded ? 'Свернуть' : 'Читать далее'}</Text>
-      </TouchableOpacity>
-    </>
+        <TouchableOpacity onPress={toggleExpanded} hitSlop={hitSlop}>
+          <Text style={s.toggleText}>{expanded ? 'Свернуть' : 'Читать далее'}</Text>
+        </TouchableOpacity>
+      </View>
+
+      {expanded && (
+        <TouchableOpacity onPress={toggleExpanded} style={s.up}>
+          <Icon name='chevron-up' />
+        </TouchableOpacity>
+      )}
+    </View>
   );
 }
 
@@ -33,6 +48,18 @@ ExpandableText.defaultProps = {
 };
 
 const s = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  up: {
+    width: 20,
+    marginLeft: 10,
+    backgroundColor: color.LightBackground,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingBottom: 7,
+  },
   body: {
     color: color.Review,
     marginTop: 10,
