4aa8cfacbab3425d7d57953c4c4c95a9412e3638
Denis Moiseev <isdenmois@gmail.com>
2020-05-08 18:05:25 +0000

    Reduced web app size

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-require('dotenv').config();
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+require('dotenv').config();
 
 const babelConfig = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  presets: [
     [
-      '@babel/plugin-transform-runtime',
+      '@babel/preset-env',
       {
-        helpers: true,
-        regenerator: true,
+        targets: 'ios 12.1',
       },
     ],
+    'module:react-native-dotenv',
+    '@babel/preset-react',
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
     [
       'module-resolver',
       {
@@ -28,45 +31,14 @@ const babelConfig = {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.web.js', '.web.tsx'],
       },
     ],
-    [
-      'babel-plugin-jsx-remove-data-test-id',
-      {
-        attributes: 'testID',
-      },
-    ],
+    ['babel-plugin-jsx-remove-data-test-id', { attributes: 'testID' }],
+    'lodash',
   ],
-};
-const flowConfig = {
-  test: /\.jsx?$/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: babelConfig.presets,
-      plugins: ['@babel/plugin-syntax-flow', '@babel/plugin-transform-flow-strip-types', ...babelConfig.plugins],
-    },
-  },
-};
-const typescriptConfig = {
-  test: /\.tsx?$/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: babelConfig.presets,
-      plugins: [
-        [
-          require('@babel/plugin-transform-typescript'),
-          {
-            isTSX: true,
-          },
-        ],
-        ...babelConfig.plugins,
-      ],
-    },
-  },
 };
 
 const rootDir = path.join(__dirname, '.');
 const webpackEnv = process.env.NODE_ENV || 'development';
+const isProd = webpackEnv === 'production';
 
 module.exports = {
   mode: webpackEnv,
@@ -76,11 +48,91 @@ module.exports = {
     filename: 'app-[hash].bundle.js',
     globalObject: 'this',
   },
-  devtool: webpackEnv === 'development' ? 'eval-source-map' : false,
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    crypto: 'empty',
+  },
+  devtool: !isProd && 'eval-source-map',
+  optimization: isProd
+    ? {
+        usedExports: true,
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: true,
+            terserOptions: {
+              output: {
+                comments: false,
+              },
+            },
+            extractComments: false,
+          }),
+        ],
+        splitChunks: {
+          cacheGroups: {
+            default: {
+              minChunks: 2,
+              reuseExistingChunk: true,
+            },
+            vendor: {
+              test: /node_modules/,
+              chunks: 'initial',
+              name: 'vendor',
+              priority: 10,
+              enforce: true,
+            },
+          },
+        },
+      }
+    : {},
   module: {
     rules: [
-      flowConfig,
-      typescriptConfig,
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            configFile: false,
+            presets: babelConfig.presets,
+            plugins: ['@babel/plugin-syntax-flow', '@babel/plugin-transform-flow-strip-types', ...babelConfig.plugins],
+          },
+        },
+      },
+      {
+        test: /\.tsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            configFile: false,
+            presets: babelConfig.presets,
+            plugins: [
+              ['@babel/plugin-transform-typescript', { isTSX: true, allowDeclareFields: true, allExtensions: true }],
+              ...babelConfig.plugins,
+            ],
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            configFile: false,
+            presets: babelConfig.presets,
+            plugins: [
+              ['@babel/plugin-transform-typescript', { isTSX: false, allowDeclareFields: true, allExtensions: true }],
+              ...babelConfig.plugins,
+            ],
+          },
+        },
+      },
       {
         test: /\.(gif|jpe?g|png|svg|ttf)$/,
         use: {
@@ -98,17 +150,29 @@ module.exports = {
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [
         path.resolve(rootDir, 'dist/assets'),
-        path.resolve(rootDir, 'dist/app-*.bundle.js'),
+        path.resolve(rootDir, 'dist/*.bundle.js'),
         path.resolve(rootDir, 'dist/*.worker.js'),
+        ...(process.env.JSON ? [] : [path.resolve(rootDir, 'dist/stats.json')]),
       ],
     }),
     new HtmlWebpackPlugin({
       template: './web/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+      },
     }),
     new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
       __DEV__: webpackEnv === 'development',
     }),
-    new webpack.HotModuleReplacementPlugin(),
+    ...(isProd ? [] : [new webpack.HotModuleReplacementPlugin()]),
+    new CopyPlugin([
+      { from: path.resolve(rootDir, 'web/apple-launch-750x1334.png'), to: 'assets' },
+      { from: path.resolve(rootDir, 'android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png'), to: 'assets' },
+    ]),
   ],
   resolve: {
     extensions: ['.web.tsx', '.web.ts', '.tsx', '.ts', '.web.jsx', '.web.js', '.jsx', '.js'], // read files in fillowing order
