dae6d7562f06473e9315133f67ecce3b75abfc6c
Denis Moiseev <isdenmois@gmail.com>
2020-07-31 14:38:22 +0000

    feat(release): added web build

--- dev-tools/release/index.js
+++ dev-tools/release/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { execSync } = require('child_process');
 const { build } = require('./gradle');
 const { upload } = require('./upload');
+const { web } = require('./web');
 
 const argv = require('yargs')
   .option('type', { default: 'release' })
@@ -9,12 +10,15 @@ const argv = require('yargs')
   .boolean('clean')
   .boolean('upload')
   .boolean('major')
+  .boolean('skip-version')
+  .option('web', { type: 'boolean', default: true })
   .help().argv;
 
-if (argv.type === 'release') {
+if (argv.type === 'release' && !argv.skipVersion) {
   execSync(`node dev-tools/changelog ${argv.major ? '--major' : ''}`, { stdio: 'inherit' });
 }
 
 build(argv.type, argv.clean, argv.test)
   .then(apk => argv.upload && upload(apk))
+  .then(() => argv.web && web(argv.type))
   .catch(error => console.error(error && error.toString()));
