fc9b4ef9e4bbcecb49e622e6fd3f2005e38c8062
Denis Moiseev <isdenmois@gmail.com>
2021-12-06 12:49:09 +0300

    fix(home): book cover svg

--- src/screens/home/components/book-edge.tsx
+++ src/screens/home/components/book-edge.tsx
@@ -1,58 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Svg, Path } from 'react-native-svg';
 import { Platform, StyleSheet } from 'react-native';
+import _ from 'lodash';
+
+const pathCreator = () =>
+  _.times(
+    7,
+    i => `M 0 ${i * 2} l ${15 - i * 2} 0 a 20 20 0 0 1 20 20 l 0 ${184 - 2 * i * 2} a 20 20 0 0 1 -20 20`,
+  ).join(' ');
 
 export const BookEdge: FC = () => {
+  const d = useMemo(pathCreator, []);
+
   return (
     <Svg height={224} width={38} style={s.svg}>
-      <Path
-        d='M 0 0 l 15 0 a 20 20 0 0 1 20 20 l 0 184 a 20 20 0 0 1 -20 20 l -15 0'
-        fill='#F3F4F1'
-        stroke='#DDD'
-        strokeWidth={0.5}
-      />
-      <Path
-        d='M 0 0 l 13 0 a 20 20 0 0 1 20 20 l 0 184 a 20 20 0 0 1 -20 20 l -13 0'
-        fill='#F3F4F1'
-        stroke='#DDD'
-        strokeWidth={0.5}
-      />
-      <Path
-        d='M 0 0 l 11 0 a 20 20 0 0 1 20 20 l 0 184 a 20 20 0 0 1 -20 20 l -11 0'
-        fill='#F3F4F1'
-        stroke='#DDD'
-        strokeWidth={0.5}
-      />
-      <Path
-        d='M 0 0 l 9 0 a 20 20 0 0 1 20 20 l 0 184 a 20 20 0 0 1 -20 20 l -9 0'
-        fill='#F3F4F1'
-        stroke='#DDD'
-        strokeWidth={0.5}
-      />
-      <Path
-        d='M 0 0 l 7 0 a 20 20 0 0 1 20 20 l 0 184 a 20 20 0 0 1 -20 20 l -7 0'
-        fill='#F3F4F1'
-        stroke='#DDD'
-        strokeWidth={0.5}
-      />
-      <Path
-        d='M 0 0 l 5 0 a 20 20 0 0 1 20 20 l 0 184 a 20 20 0 0 1 -20 20 l -5 0'
-        fill='#F3F4F1'
-        stroke='#DDD'
-        strokeWidth={0.5}
-      />
-      <Path
-        d='M 0 0 l 3 0 a 20 20 0 0 1 20 20 l 0 184 a 20 20 0 0 1 -20 20 l -3 0'
-        fill='#F3F4F1'
-        stroke='#DDD'
-        strokeWidth={0.5}
-      />
-      <Path
-        d='M 0 0 l 1 0 a 20 20 0 0 1 20 20 l 0 184 a 20 20 0 0 1 -20 20 l -1 0'
-        fill='#F3F4F1'
-        stroke='#DDD'
-        strokeWidth={0.5}
-      />
+      <Path d={d} stroke='#DDD' strokeWidth={0.5} />
     </Svg>
   );
 };
@@ -60,8 +22,10 @@ export const BookEdge: FC = () => {
 const s = StyleSheet.create({
   svg: {
     marginLeft: -20,
+    zIndex: 0,
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
+    backgroundColor: '#F3F4F1',
     ...Platform.select({
       android: {
         elevation: 2,
--- src/screens/home/components/now-reading-book.tsx
+++ src/screens/home/components/now-reading-book.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { StyleSheet, ImageStyle, TouchableOpacity, Platform } from 'react-native';
+import { StyleSheet, ImageStyle, TouchableOpacity, Platform, View } from 'react-native';
 import Book from 'store/book';
 import { BOOK_STATUSES } from 'types/book-statuses.enum';
 import { Button2, Thumbnail } from 'components';
@@ -22,6 +22,7 @@ export function NowReadingBook({ book }: Props) {
   return (
     <Box alignItems='center' mt={3}>
       <TouchableOpacity style={s.thumbnail} onPress={openBook} testID='currentThumbnail'>
+        <View style={s.imageBackground} />
         <Thumbnail cache style={s.image} width={150} height={225} title={book.title} url={book.thumbnail} />
 
         <BookEdge />
@@ -39,13 +40,26 @@ export function NowReadingBook({ book }: Props) {
 }
 
 const s = StyleSheet.create({
+  imageBackground: {
+    position: 'absolute',
+    width: 130,
+    height: 225,
+    left: 0,
+    top: 0,
+    backgroundColor: '#F3F4F1',
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+    zIndex: 2,
+  },
   thumbnail: {
     flexDirection: 'row',
     width: 166,
+    backgroundColor: '#F3F4F1',
+    borderRadius: 20,
   } as ImageStyle,
   image: {
     borderRadius: 20,
-    zIndex: 2,
+    zIndex: 3,
     ...Platform.select({
       android: {
         elevation: 2,
