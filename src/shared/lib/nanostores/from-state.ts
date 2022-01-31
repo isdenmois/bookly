d2df2b5fff72a3b296e7ebb25c49c2f5d3b91dfb
Denis Moiseev <isdenmois@gmail.com>
2022-01-31 10:10:02 +0300

    Fix fromState util

--- src/shared/lib/nanostores/from-state.ts
+++ src/shared/lib/nanostores/from-state.ts
@@ -4,11 +4,15 @@ import { State } from 'utils/state';
 export function fromState<T, K extends keyof T>(state: State<T>, field: K): ReadableAtom<T[K]> {
   const store = atom<T[K]>(state[field]);
 
-  onMount(store, () =>
+  store.set(state.getState()[field]);
+
+  onMount(store, () => {
+    store.set(state.getState()[field]);
+
     state.watch(field, value => {
       store.set(value);
-    }),
-  );
+    });
+  });
 
   return store;
 }
